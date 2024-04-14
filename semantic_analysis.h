114adf61bde24d5db6b3baffe1a7f59e761d0602
Henry S. Bonnie <f004gd9@babylon3.thayer.dartmouth.edu>
2024-04-14 19:58:13 -0400

    test

--- semantic_analysis.h
+++ semantic_analysis.h
@@ -4,7 +4,12 @@
 #include <vector>
 #include <stack>
 using namespace std;
+/*
+    Analyze Node function
 
+    Parameters
+
+*/
 int analyze_node(astNode* node, vector<vector<char*>*>* symbol_tables);
 
 int analyze_stmt(astStmt* stmt, vector<vector<char*>*>* symbol_tables);
